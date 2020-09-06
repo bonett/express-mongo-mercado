a48be06b7cb6fe26cb5729a7ddaffc2fe5c5b9f9
Wilfrido Bonett <wbonett10@gmail.com>
2020-09-06 12:05:04 -0500

    categories and items is working

--- controllers/items.js
+++ controllers/items.js
@@ -7,35 +7,46 @@ const API_URL = `${process.env.API}sites/MLA/`;
 module.exports = {
   index: async (req, res) => {
     /* const queryString = res.query.q; */
-    const queryString = "ihpone";
+    const queryString = "apple";
     Request.get(
       `${API_URL}search?q=${queryString}`,
       (error, response, body) => {
         try {
           const data = JSON.parse(body);
-          const items = _.map(data.results, (item) => {
-            return _.reduce(data.results, (result, item) => {
-              return {
-                id: item.id,
-                title: item.title,
-                price: {
-                  currency: item.installments.currency_id,
-                  amount: item.installments.amount,
-                  decimals: item.installments.currency_id,
-                },
-                picture: item.thumbnail,
-                condition: item.condition,
-                free_shipping: item.free_shipping,
-              };
-            });
-          });
+          let filters = [];
+          let paths = [];
+          let categories = [];
 
+          if (data.filters.length !== 0) {
+            filters = data.filters && data.filters[0];
+            paths = filters.values && filters.values[0];
+            categories = _.map(paths.path_from_root, (path) => {
+              return path.name;
+            });
+          } else {
+            categories.push(queryString)
+          }
+
+          const items = _.map(data.results, (item) => {
+            return {
+              id: item.id,
+              title: item.title,
+              price: {
+                currency: item.currency_id,
+                amount: item.price,
+                decimals: item.installments.currency_id,
+              },
+              picture: item.thumbnail,
+              condition: item.condition,
+              free_shipping: item.free_shipping,
+            };
+          });
           const response = {
             author: {
               name: "Wilfrido",
               lastname: "Bonett",
             },
-            categories: [],
+            categories,
             items,
           };
           res.status(200).json(response);
