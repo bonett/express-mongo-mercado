b6f292180a54e6332aa7d4449391237bed14df74
Wilfrido Bonett <wbonett10@gmail.com>
2020-09-06 10:59:51 -0500

    response by query

--- controllers/items.js
+++ controllers/items.js
@@ -1,22 +1,50 @@
-require('dotenv').config();
-const Request = require('request');
-const API_URL = `${process.env.API}sites/MLA/`
+require("dotenv").config();
+const _ = require("lodash");
+
+const Request = require("request");
+const API_URL = `${process.env.API}sites/MLA/`;
 
 module.exports = {
+  index: async (req, res) => {
+    /* const queryString = res.query.q; */
+    const queryString = "ihpone";
+    Request.get(
+      `${API_URL}search?q=${queryString}`,
+      (error, response, body) => {
+        try {
+          const data = JSON.parse(body);
+          const items = _.map(data.results, (item) => {
+            return _.reduce(data.results, (result, item) => {
+              return {
+                id: item.id,
+                title: item.title,
+                price: {
+                  currency: item.installments.currency_id,
+                  amount: item.installments.amount,
+                  decimals: item.installments.currency_id,
+                },
+                picture: item.thumbnail,
+                condition: item.condition,
+                free_shipping: item.free_shipping,
+              };
+            });
+          });
 
-    index: async (req, res) => {
-        /* const queryString = res.query.q; */
-        const queryString = 'ihpone';
-        Request.get(`${API_URL}search?q=${queryString}`, (error, response, body) => {
-            try {
-                res.status(200).json({
-                    result: JSON.parse(body)
-                });
-            } catch (err) {
-                res.status(500).json({
-                    message: err.message
-                });
-            }
-        });
-    }
-}
+          const response = {
+            author: {
+              name: "Wilfrido",
+              lastname: "Bonett",
+            },
+            categories: [],
+            items,
+          };
+          res.status(200).json(response);
+        } catch (err) {
+          res.status(500).json({
+            message: err.message,
+          });
+        }
+      }
+    );
+  },
+};
